9466b78700d6f47125f0078b426a931d9f0e84e8
alirezadadashi1995 <alirezadadashi1995@gmail.com>
2025-05-03 15:53:52 +0330

    Update script.js

--- script.js
+++ script.js
@@ -144,12 +144,4 @@ function checkRepairStatus() {
     });
 }
 
-// نمایش آیکون پس از اسکرول کاربر
-window.addEventListener('scroll', function() {
-    const whatsappSupport = document.querySelector('.whatsapp-support');
-    if (window.scrollY > 0) {
-        whatsappSupport.style.display = 'block';
-    } else {
-        whatsappSupport.style.display = 'none';
-    }
-});
+
