91b7458e118d49b3b1fccc9b0f7f85012d2ce357
alirezadadashi1995 <alirezadadashi1995@gmail.com>
2025-05-04 15:30:54 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,5 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-const totalSlides = slides.length;
-const prevButton = document.getElementById("prevSlide");
-const nextButton = document.getElementById("nextSlide");
 
-function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
-}
-
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-}
-
-nextButton.addEventListener("click", nextSlide);
-prevButton.addEventListener("click", prevSlide);
-
-// نمایش خودکار هر ۳ ثانیه
-setInterval(nextSlide, 3000);
-
-// نمایش اسلاید اول در ابتدا
-showSlide(currentSlide);
-let startX = 0;
-let endX = 0;
-
-document.querySelector(".slider").addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-});
-
-document.querySelector(".slider").addEventListener("touchend", (e) => {
-    endX = e.changedTouches[0].clientX;
-    if (startX > endX + 50) {
-        nextSlide(); // سوایپ به چپ (نمایش اسلاید بعدی)
-    } else if (startX < endX - 50) {
-        prevSlide(); // سوایپ به راست (نمایش اسلاید قبلی)
-    }
-});
-
-document.querySelectorAll('.main-nav ul li a').forEach(link => {
-    link.addEventListener('click', () => {
-        toggleMenu();
-    });
-});
-
-document.getElementById("trackingCode").addEventListener("keypress", function(e) {
+  document.getElementById("trackingCode").addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         checkRepairStatus(); // جستجو به محض فشردن اینتر
     }
@@ -143,13 +93,3 @@ function checkRepairStatus() {
         document.getElementById("repairCost").innerHTML = "";
     });
 }
-
-// نمایش آیکون پس از اسکرول کاربر
-window.addEventListener('scroll', function() {
-    const whatsappSupport = document.querySelector('.whatsapp-support');
-    if (window.scrollY > 300) {
-        whatsappSupport.style.display = 'block';
-    } else {
-        whatsappSupport.style.display = 'none';
-    }
-});
